6e82ec0b6691edf8f0ae5afbfa7584a7cdfc6356
alfannn785 <alfanalhisyam.m.adibs@gmail.com>
2025-12-18 22:36:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ function playAudio(type) {
     audio.src = "eat.mp3";
   } else if (type === "drink") {
     audio.src = "drink.mp3";
+  } else if (type === "hello.mp3") {
+    audio.src = "helo.mp3";
   }
 
   audio.play();
 }
 
   
+
